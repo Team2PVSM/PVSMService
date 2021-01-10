dddda5817a13fce61e4db7e8cdc6fdb2e4792aa0
unknown <dharmik3504>
2021-01-10 09:28:04 +0530

    up

---
+++ PassportVisaManagementSystemService/Model/ApplyVisa.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace PassportVisaManagementSystemService.Model
+{
+    public class ApplyVisa
+    {
+        [DataMember]
+        public int Id { set; get; }
+
+        [DataMember]
+        public User User { set; get; }
+
+        [DataMember]
+        public Country Countries { set; get; }
+        [DataMember]
+        public string Occupation { set; get; }
+
+        [DataMember]
+        public DateTime DateOfApplication { set; get; }
+
+        [DataMember]
+        public DateTime DateOfExpiry { set; get; }
+
+        [DataMember]
+        public DateTime VisaNumber { set; get; }
+
+        [DataMember]
+        public DateTime DateOfIssue { set; get; }
+
+        [DataMember]
+        public double RegistrationCost { set; get; }
+
+        
+
+
+
+
+
+
+
+
+    }
+}
