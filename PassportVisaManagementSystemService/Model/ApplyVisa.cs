6caf48f27ebb640d3a23cb6044eb379946ac64d2
unknown <dharmik3504>
2021-01-10 09:51:31 +0530

    up

--- PassportVisaManagementSystemService/Model/ApplyVisa.cs
+++ PassportVisaManagementSystemService/Model/ApplyVisa.cs
@@ -34,7 +34,9 @@ namespace PassportVisaManagementSystemService.Model
         [DataMember]
         public double RegistrationCost { set; get; }
 
-        
+        [DataMember]
+        public string status { set; get; }
+
 
 
 
