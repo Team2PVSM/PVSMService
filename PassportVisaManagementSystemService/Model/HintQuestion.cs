048e2901a10f36f55e346c33c3eb871764b5fb5a
Vin <vineetgaur97@gmail.com>
2021-01-10 08:57:08 +0530

    Model Change

---
+++ PassportVisaManagementSystemService/Model/HintQuestion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace PassportVisaManagementSystemService.Model
+{
+    public class HintQuestion
+    {
+        [DataMember]
+        public int Id { set; get; }
+        public string Questions { set; get; }
+
+    }
+}
---
+++ PassportVisaManagementSystemService/Model/User.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace PassportVisaManagementSystemService.Model
+{
+    public class User
+    {
+        //[Key]
+        [DataMember]
+        public int Id { set; get; }
+
+        [DataMember]
+        public int UserId { set; get; }
+       
+        [DataMember]
+        public string FirstName { set; get; }
+
+        [DataMember]
+        public string SurName { set; get; }
+
+        [DataMember]
+        public DateTime DateOfBirth { set; get; }
+
+        [DataMember]
+        public string Address { set; get; }
+
+        //[DataMember]
+        //public Country Country { set; get; }
+
+        [DataMember]
+        public int	ContactNo { set; get; }
+
+        [DataMember]
+        public string EmailAddress { set; get; }
+
+        [DataMember]
+        public string Qualification { set; get; }
+
+        [DataMember]
+        public string Gender { set; get; }
+
+        [DataMember]
+        public string ApplyType { set; get; }
+
+        [DataMember]
+        public HintQuestion HintQuestion { set; get; }
+
+        [DataMember]
+        public string HintAnswer { set; get; }
+
+        [DataMember]
+        public string CitizenType { set; get; }
+
+        [DataMember]
+        public string Password { set; get; }
+    }
+}
